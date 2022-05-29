85ef5507750d23b76843a96dd149a913ed885109
Olaoluwa Raji <olaoluwaraji1999@gmail.com>
2022-05-29 21:28:30 +0100

    Update
    1. Added code snippet to test Bluetooth communication between K-Bot and app.
    2. Added Android APK file for the app developed using MIT App Inventor

--- K_Bot/src/main.cpp
+++ K_Bot/src/main.cpp
@@ -1,3 +1,4 @@
+#include "BluetoothSerial.h"
 #include "outputDev.h"
 
 //Output devices driven by L298N module
@@ -5,23 +6,62 @@ OutputDev pump(0,5);
 OutputDev brush(1,23);
 OutputDev leftWheel(2,15);
 OutputDev rightWheel(3,17);
+//Inbuilt Bluetooth
+BluetoothSerial bluetooth;
+
+static void ProcessAppData(char appData)
+{
+  static bool togglePump;
+  static bool toggleBrush;
+  switch(appData)
+  {
+    case '0': //Pump control
+      togglePump ^= true;
+      pump.WritePin(togglePump);
+      break;
+    case '1': //Brush control
+      toggleBrush ^= true;
+      brush.WritePin(toggleBrush);
+      break;
+    case '2': //Move Back
+      break;
+    case '3': //Turn Left
+      break;
+    case '4': //Stop
+      leftWheel.WritePins(false,false);
+      rightWheel.WritePins(false,false);
+      break;
+    case '5': //Turn Right
+      break;
+    case '6': //Move Forward
+      break;
+  }
+}
 
 void setup() 
 {
+  setCpuFrequencyMhz(80);
+  bluetooth.begin("K-Bot");
+  //Enable PWM for the actuators with appropriate duty cycle (in %)
+  pump.EnablePWM(90);
+  brush.EnablePWM(55);
+  leftWheel.EnablePWM(65);
+  rightWheel.EnablePWM(65);
+  
+  //Serial debug
+  Serial.begin(9600);
 }
 
 void loop() 
 {
-  //Testing the actuators
-  pump.EnablePWM(35);
-  brush.EnablePWM(50);
-  leftWheel.EnablePWM(75);
-  rightWheel.EnablePWM(100);
-  delay(5000);
-
-  pump.DisablePWM();
-  brush.DisablePWM();
-  leftWheel.DisablePWM();
-  rightWheel.DisablePWM();
-  delay(5000);
+  //Testing bluetooth communication with app
+  if(bluetooth.available())
+  {
+    char appData = bluetooth.read();
+    //======== Debug ===============
+    Serial.print("Received = ");
+    Serial.println(appData);
+    //==============================
+    ProcessAppData(appData);
+  }
 }
