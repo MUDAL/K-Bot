e1b4799b934222b9967268e796490ccb8f56b78c
Olaoluwa Raji <olaoluwaraji1999@gmail.com>
2022-08-21 20:22:47 +0100

    Update K_Bot.ino

--- K_Bot/K_Bot.ino
+++ K_Bot/K_Bot.ino
@@ -1,17 +1,30 @@
 #include <SoftwareSerial.h>
 #include <Servo.h>
 #include "L298N_Dev.h"
+
+//The K-Bot is a floor cleaning device that can be controlled through
+//Bluetooth communication with a mobile app (developed using MIT App
+//Inventor).
+
 //Defines
-#define RX_PIN                 2
-#define TX_PIN                -1
+#define RX_PIN                 2 //connected to HC05 Tx pin
+#define TX_PIN                -1 //no need to connect HC05 Rx pin to valid Arduino pin
 #define SERVO_POS_DELAY     1500
+#define SERVO_PIN              9
+#define PUMP_PIN               8
+#define LEFT_WHEEL_PIN1        5
+#define LEFT_WHEEL_PIN2        6
+#define LEFT_WHEEL_PWM_PIN     3
+#define RIGHT_WHEEL_PIN1       A2
+#define RIGHT_WHEEL_PIN2       A3
+#define RIGHT_WHEEL_PWM_PIN    11
 //Bluetooth
 SoftwareSerial bluetooth = SoftwareSerial(RX_PIN,TX_PIN);
 //Output devices driven by L298N module
-L298N_Dev pump(8); 
-L298N_Dev leftWheel(5,6,3); 
-L298N_Dev rightWheel(A2,A3,11); 
-//Servo (Brush/cleaning pad is attached to servo arm)
+L298N_Dev pump(PUMP_PIN); 
+L298N_Dev leftWheel(LEFT_WHEEL_PIN1,LEFT_WHEEL_PIN2,LEFT_WHEEL_PWM_PIN); 
+L298N_Dev rightWheel(RIGHT_WHEEL_PIN1,RIGHT_WHEEL_PIN2,RIGHT_WHEEL_PWM_PIN); 
+//Servo (Brush/cleaning pad/cloth is attached to a shaft connected to the servo)
 Servo servo;
 static bool servoActivated;
 static uint32_t servoStartTime;
@@ -68,7 +81,7 @@ void setup(void)
 {
   pinMode(RX_PIN,INPUT);
   bluetooth.begin(9600);
-  servo.attach(9);
+  servo.attach(SERVO_PIN);
   //switch output devices off
   pump.Write(0);
   leftWheel.SetSpeed(0);
@@ -87,7 +100,7 @@ void loop(void)
   if(servoActivated && ((millis() - servoStartTime) >= SERVO_POS_DELAY))
   {
     static int i;
-    const int servoPos[] = {0,180};
+    const int servoPos[] = {0,180}; //extreme servo positions
     servo.write(servoPos[i]);
     servoStartTime = millis();
     i ^= 1;
